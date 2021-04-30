1731b51a5ac3c82b2f3693fe3d8c53136993cbde
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-04-30 23:46:25 +0530

    Create codeplay.cpp

---
+++ Codechef/Lunch Time/April2021/codeplay.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/LTIME95C/problems/SLOOP
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m,s;
+	    cin>>m>>s;
+	    cout<<m/s<<"\n";
+	}
+	return 0;
+}
