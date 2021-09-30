ffce811679d71b365d704ba7e8137778c5ab1cd1
Akhilesh Mehra <60135972+Akhilesh2002@users.noreply.github.com>
2021-09-30 14:24:35 +0530

    Create Red Alert.cpp

---
+++ Codechef/Lunch Time/July 2021/Red Alert.cpp
@@ -0,0 +1,113 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <cstring>
+#include <bitset>
+#include <random>
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+//typedef: 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<p32> vp32;
+typedef vector<p64> vp64;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef unordered_map<int,int> um32;
+typedef unordered_map<ll,ll> um64;
+ll MOD = 998244353;
+ll modulo = 1000000007;
+double eps = 1e-12;
+//define:
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define foren(i,e) for(ll i = 0; i <= e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define forsen(i,s,e) for(ll i = s; i <= e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define sorta(k) sort(k.begin(),k.end())
+#define sortd(k) sort(k.begin(),k.end(),greater<int>())
+#define rev(k) reverse(k.begin(),k.end())
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+//function:
+ 
+//main:
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n , d , h;
+        cin >> n >> d >> h;
+        ll arr[n];
+        ll waterLevel = 0;
+        bool flag = false;
+        forn (i , n)
+        {
+            cin >> arr[i];
+        }
+        forn (i , n)
+        {
+            if (arr[i] > 0)
+            {
+                waterLevel += arr[i];
+            }
+            else 
+            {
+                if (waterLevel < d)
+                    waterLevel = 0;
+                else
+                    waterLevel -= d;
+            }
+            if (waterLevel > h)
+            {
+                cout << "YES";
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            cout << "NO";
+        cout << endl;
+    }
+    return 0;
+}
